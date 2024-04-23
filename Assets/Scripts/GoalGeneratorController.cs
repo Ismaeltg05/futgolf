9079ac2d6fa9ff74700b2f79d9a16f6ef57255a5
mrdarip <darioelas2005@gmail.com>
2024-04-23 13:08:23 +0200

    Added Start GoalGeneratorController.cs and its goal generation logic

--- Assets/Scripts/GoalGeneratorController.cs
+++ Assets/Scripts/GoalGeneratorController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GoalGeneratorController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Goal goalAsset;
+    private Goal goal;
+    private BoxCollider validArea;
     void Start()
     {
-        
+        validArea = GetComponent<BoxCollider>();
+        goal = Instantiate(goalAsset,getNextGoalPosition(),Quaternion.identity,null);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 getNextGoalPosition()
     {
-        
+        RaycastHit hit;
+        if(
+            Physics.Raycast(RandomPointInBounds(validArea.bounds),Vector3.down, out hit,float.MaxValue)
+            )
+        {
+            return hit.point;
+        }
+
+        Debug.LogWarning("No se encontró donde poner una portería, lanzando otro rayo");
+        return getNextGoalPosition();
+    }
+
+    public static Vector3 RandomPointInBounds(Bounds bounds)
+    {
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
     }
 }
