a4c3e233ac1f9a36ac893ea5f31a1e72157c084c
aguila232323 <159147580+aguila232323@users.noreply.github.com>
2024-04-23 13:14:43 +0200

    GeneralScore

---
+++ Assets/Scripts/ScoreTable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreTable : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro[] _PlayerScores;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
