3c88880b61bf3826c41c8b384449b907fc151cdc
aguila232323 <159147580+aguila232323@users.noreply.github.com>
2024-04-24 10:02:19 +0200

    PlayerScore ArrayFix

--- Assets/Scripts/ScoreTable.cs
+++ Assets/Scripts/ScoreTable.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreTable : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro[] _PlayerScores;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private TextMeshProUGUI[] _PlayerScoresText = new TextMeshProUGUI[4];
+    
     void Update()
     {
         
+
     }
 }
