50a626a8f2d0b730515fc871de7753ad1f8bfb03
xXSalm3ronXx <fransalmeronpuig123@gmail.com>
2024-04-24 10:37:48 +0200

    Camera

---
+++ Assets/Movement_Camera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class Movement_Camera : MonoBehaviour
+{
+    private Cinemachine3rdPersonAim cinemachine3RdPersonAim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
