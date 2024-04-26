73f5007496220db3ca7e73c74291d71e5b491101
xXSalm3ronXx <fransalmeronpuig123@gmail.com>
2024-04-26 12:44:05 +0200

    Ghost

---
+++ Assets/Persecutor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Persecutor : MonoBehaviour
+{
+    [SerializeField] private TurnManager turnManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = new Vector3(turnManager.players[0].GetComponent<Transform>().position.x,turnManager.players[0].GetComponent<Transform>().position.y,turnManager.players[0].GetComponent<Transform>().position.z);
+    }
+}
