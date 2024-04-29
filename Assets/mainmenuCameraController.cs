93b48de961ab1665b30402095711b95477f499f9
mrdarip <darioelas2005@gmail.com>
2024-04-29 13:30:12 +0200

    decorated the main menu scene

---
+++ Assets/mainmenuCameraController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenuCameraController : MonoBehaviour
+{
+    Vector3 startPos;
+    Vector3 startRot;
+    void Update()
+    {
+        transform.position = startPos + new Vector3(Mathf.PerlinNoise1D(Time.time * 0.15f), Mathf.PerlinNoise1D(Time.time *0.1f), Mathf.PerlinNoise1D(Time.time * 0.13f)) *3f;
+    }
+}
