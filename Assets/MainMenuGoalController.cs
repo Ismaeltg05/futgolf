26abd95c1a41dedfffb8364708dadeca33a4e10e
mrdarip <darioelas2005@gmail.com>
2024-04-29 11:42:12 +0200

    fixed particle system in main menu, created a messi particle and added more balls

--- Assets/MainMenuGoalController.cs
+++ Assets/MainMenuGoalController.cs
@@ -4,18 +4,4 @@ using UnityEngine;
 
 public class MainMenuGoalController : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem particleSystem;
-    ParticleSystem.ShapeModule _editableShape;
-    private void Start()
-    {
-        _editableShape = particleSystem.shape;
-    }
-
-    public void generateParticles(Vector3 position, Vector3 rotation) { 
-        print("Collision");
-        _editableShape.position = transform.position - position - Vector3.forward * transform.localScale.z;
-        _editableShape.rotation = rotation;
-
-        particleSystem.Emit(100);
-    }
 }
